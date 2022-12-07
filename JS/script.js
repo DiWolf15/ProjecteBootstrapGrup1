d9941e95d3970e90a1533465beb0af33c2dc3aad
DiWolf15 <unaiclap15@gmail.com>
2022-12-07 17:06:13 +0100

    1.6.3

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,13 @@
 var header = document.getElementById("titol");
 header.innerHTML = `
-					<nav class="navbar navbar-expand-lg background ">
-						<div class="container-fluid">
+					<nav class="navbar navbar-expand-lg background">
+						<div class="container-fluid p-0">
 						  <a class="navbar-brand" href="#">EXERCICIS GYM</a>
 						  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 							<span class="navbar-toggler-icon"></span>
 						  </button>
 						  <div class="collapse navbar-collapse" id="navbarSupportedContent">
 							<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-							
 							  <li class="nav-item">
 								<a class="nav-link background" href="HTML/shop.html">Shop</a>
 							  </li>
