cc1832e59ca0483d5e40cfd88a4ed15d24701247
DiWolf15 <unaiclap15@gmail.com>
2022-12-07 17:21:08 +0100

    1.7.5

--- JS/script2.js
+++ JS/script2.js
@@ -9,9 +9,9 @@ header.innerHTML = `
 						  <div class="collapse navbar-collapse" id="navbarSupportedContent">
 							<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 							  <li class="nav-item">
-								<a class="nav-link background" href="index.html">Home</a>
+								<a class="nav-link background" href="../index.html">Home</a>
 							  </li>
-								<li class="nav-item">
+							  <li class="nav-item">
 								<a class="nav-link background" href="HTML/shop.html">Shop</a>
 							  </li>
 							  <li class="nav-item">
