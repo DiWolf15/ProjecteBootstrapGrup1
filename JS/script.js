77c94143b655078a56ef8bf5cd37051074d61fb6
DiWolf15 <unaiclap15@gmail.com>
2022-12-07 16:16:48 +0100

    1.5.6

--- JS/script.js
+++ JS/script.js
@@ -25,5 +25,34 @@ header.innerHTML = `
 						</div>
 					  </nav>
 				
-`;
+`
+var titol2 = document.getElementById("titol2");
+titol2.innerHTML = `
+					<nav class="navbar navbar-expand-lg background ">
+						<div class="container-fluid">
+						  <a class="navbar-brand" href="#">EXERCICIS GYM</a>
+						  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="navbar-toggler-icon"></span>
+						  </button>
+						  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+							<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+							  <li class="nav-item">
+								<a class="nav-link active background" aria-current="page" href="../index.html">Home</a>
+							  </li>
+							  <li class="nav-item">
+								<a class="nav-link background" href="HTML/shop.html">Shop</a>
+							  </li>
+							  <li class="nav-item">
+								<a class="nav-link disabled background" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+							  </li>
+							</ul>
+							<div>
+								<img src="https://img.freepik.com/premium-photo/3d-rendering-black-dumbbells-floor-dark-concept-fitness-room-with-training-equipments-back_67155-14961.jpg" width="800px" height="100px">
+							</div>
+						  </div>
+						</div>
+					  </nav>
+				
+`
+;
 
